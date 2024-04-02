112a9b4a4a7864d00db923463279c602bae15b55
gusvasquez <gustavo.vasquez@corhuila.edu.co>
2024-04-02 17:44:34 -0500

    Fin de la clase Curves

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:disenos_app/src/pages/headers_page.dart';
+import 'package:disenos_app/src/pages/animaciones_page.dart';
+//import 'package:disenos_app/src/pages/headers_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: HeadersPage(),
+      home: AnimacionesPage(),
     );
   }
 }
---
+++ lib/src/pages/animaciones_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
+class AnimacionesPage extends StatelessWidget {
+  const AnimacionesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _CuadradoAnimado(),
+      ),
+    );
+  }
+}
+
+class _CuadradoAnimado extends StatefulWidget {
+  @override
+  State<_CuadradoAnimado> createState() => _CuadradoAnimadoState();
+}
+
+class _CuadradoAnimadoState extends State<_CuadradoAnimado>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotacion;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 4000));
+
+    rotacion = Tween(begin: 0.0, end: 2 * Math.pi)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
+
+    controller.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        controller.reset();
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Reproduccion
+    controller.forward();
+    return AnimatedBuilder(
+      animation: controller,
+      //child: _Rectangulo(),
+      builder: (context, child) {
+        return Transform.rotate(angle: rotacion.value, child: _Rectangulo());
+      },
+    );
+  }
+}
+
+class _Rectangulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: const BoxDecoration(color: Colors.blue),
+    );
+  }
+}
